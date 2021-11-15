301f29f8816569c28aa6c594eee1d872a85766a8
Jeremi Surzyn <57301686+14tp00@users.noreply.github.com>
2021-11-15 11:59:05 +0000

    klasy

---
+++ klasy.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+  int a,b,c;cin>>a>>b>>c;
+  
+  if((a>=1&&a<=50)&&(b>=1&&b<=50)&&(c>=1&&c<=50))
+  {
+    cout<<a<<" "<<b<<" "<<c<<"\n"
+      "a "<<a<<"\nb "<<b<<"\nc "<<c;
+  }    
+  return 0;
+}
