2ac18748d098a17576444ea55eb562144d233e41
Jeremi Surzyn <57301686+14tp00@users.noreply.github.com>
2021-11-16 10:50:53 +0000

    aemstr hcjcutfig

---
+++ wibonazzi.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0); cout.tie(0);
+
+  int n;
+  cin>>n;
+
+  int arr[n];
+
+  arr[0] = 1;
+  arr[1] = 1;
+
+  for(int i = 2;i<n;i++)
+  {
+    arr[i] = arr[i-1]+arr[i-2];
+  }
+
+  cout<<arr[n-1];
+
+  return 0;
+}
